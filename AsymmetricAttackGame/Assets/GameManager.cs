39d982107fed5c907cb182ef7d27da43be608423
BolajiMakinde <bmakinde@calpoly.edu>
2023-11-11 03:47:49 -0800

    test for build

--- AsymmetricAttackGame/Assets/GameManager.cs
+++ AsymmetricAttackGame/Assets/GameManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isWebPlayer;
+    public GameObject webPlayer;
+    public GameObject hmdPlayer;
+    public string m_DeviceType;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetType();
+        if(m_DeviceType == "Desktop" || m_DeviceType == "Console") {
+            isWebPlayer = true;
+        }
+        else {
+            isWebPlayer = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GetType() {
+        //Output the device type to the console window
+        UnityEngine.Debug.Log("Device type : " + m_DeviceType);
+        //Check if the device running this is a console
+        if (SystemInfo.deviceType == DeviceType.Console)
+        {
+            //Change the text of the label
+            m_DeviceType = "Console";
+        }
+
+        //Check if the device running this is a desktop
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            m_DeviceType = "Desktop";
+        }
+
+        //Check if the device running this is a handheld
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            m_DeviceType = "Handheld";
+        }
+
+        //Check if the device running this is unknown
+        if (SystemInfo.deviceType == DeviceType.Unknown)
+        {
+            m_DeviceType = "Unknown";
+        }
     }
 }
--- AsymmetricAttackGame/Assets/NetworkManager.cs
+++ AsymmetricAttackGame/Assets/NetworkManager.cs
@@ -15,7 +15,9 @@ public class NetworkManager : MonoBehaviour
     public SocketIOUnity socket;
     public String m_DeviceType;
     public String m_UriAddress = "http://192.168.1.107:11100";
+    public String m_UserName = "Hello";
     public Text ReceivedText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -60,9 +62,11 @@ public class NetworkManager : MonoBehaviour
 
         UnityEngine.Debug.Log("Connecting...");
         socket.Connect();
+
         ReceivedText.text = "";
         socket.OnAnyInUnityThread((name, response) =>
         {
+ 
             //String x = response.GetValue() + "\n";
             //var x = response.GetValue<String>();
             String x = response.ToString();
@@ -73,6 +77,41 @@ public class NetworkManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetType();
+        socket.Emit("type", m_DeviceType);
+        socket.Emit("name", m_UserName);
+        if(name == "") {
+
+        }
+        else if (name == "") {
+
+        }
+    }
+
+    public static bool IsJSON(string str)
+    {
+        if (string.IsNullOrWhiteSpace(str)) { return false; }
+        str = str.Trim();
+        if ((str.StartsWith("{") && str.EndsWith("}")) || //For object
+            (str.StartsWith("[") && str.EndsWith("]"))) //For array
+        {
+            try
+            {
+                var obj = JToken.Parse(str);
+                return true;
+            }catch (Exception ex) //some other exception
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void GetType() {
         //Output the device type to the console window
         UnityEngine.Debug.Log("Device type : " + m_DeviceType);
         //Check if the device running this is a console
